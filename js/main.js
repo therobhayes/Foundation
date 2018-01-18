43a0263c842a2c6e7dde5df0ffdce4736550c53b
Rondie Li <mr.rondie@gmail.com>
2018-01-18 14:35:20 -0500

    Revision Jan 18 #1
    - Updated sponsor icons
    - Updated Foundation logo
    - Cleared up margins
    - Replaced timeline with check marks
    - Some minor CSS updates

--- js/main.js
+++ js/main.js
@@ -1,63 +1,15 @@
+/*
+
 window.onscroll = function() {
     var tl = document.querySelector('.timestamp');
-    
+
     //section three animation trigger
     if ( isInViewport( tl ) ) {
         timeline();
     }
 };
 
-
-//timeline animation
-        var timeline = (function() {
-            var executed = false;
-            return function() {
-                if (!executed) {
-                    executed = true;
-
-                    var draw = document.getElementsByClassName('draw');
-                    var one = draw[0];
-                    var two = draw[1];
-                    var three = draw[2];
-
-                    setInterval(function(){
-                        $('head').append('<style>#three li:nth-child(1) .item-desc:before{border:4px solid #4717F6;background-color:#4717F6;}</style>');
-                        one.style.height = '247.5px';
-                    }, 0);
-                    setInterval(function(){
-                            $('head').append('<style>#three li:nth-child(2) .item-desc:before{border:4px solid #4717F6;background-color:#4717F6;}</style>'); 
-                            two.style.height = '247.5px';
-                        }, 900);
-                    setInterval(function(){
-                                $('head').append('<style>#three li:nth-child(3) .item-desc:before{border:4px solid #4717F6;background-color:#4717F6;}</style>');
-                                three.style.height = '247.5px';
-                            }, 1800);
-                    setInterval(function(){
-                        $('head').append('<style>#three li:nth-child(4) .item-desc:before{border:4px solid #4717F6;background-color:#4717F6;}</style>');
-                        intrvl=0;
-                        for(nTimes=0;nTimes<3;nTimes++){
-                            intrvl += 500;
-                            setTimeout("$('head').append('<style>#three li:nth-child(4) .item-desc:before{border:4px solid #e3e3e3;background-color:white;}</style>');",intrvl);
-                            intrvl += 900;
-                            setTimeout("$('head').append('<style>#three li:nth-child(4) .item-desc:before{border:4px solid #4717F6;background-color:#4717F6;}</style>');",intrvl);
-                           }
-                    }, 2700);
-                }
-            };
-        })();
-
-//detect page section position
-var isInViewport = function (elem) {
-    var bounding = elem.getBoundingClientRect();
-    return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-
-};
-
+*/
 
 // init controller
 	var controller = new ScrollMagic.Controller();
@@ -66,37 +18,41 @@ var isInViewport = function (elem) {
 
   var logoTimeline = new TimelineMax();
 			logoTimeline.to('#logo-word', .75, {autoAlpha: 0, y: "-10"}, 0)
-			 .to('#logo-corner', 1, {fill: "#4717F6"}, 0.2)
+        .to('#logo-square', 0.75, {fill: "#4717F6"}, 0.75)
+			  .to('#logo-corner', 0.5, {fill: "#ffffff"}, 1)
+        .to('#logo-shade', 1, {fill: "#3812C4"}, 3)
+
+
 
 	new ScrollMagic.Scene({offset: -300, triggerHook: 0, triggerElement: '#two'})
-					.setTween(logoTimeline) // trigger a TweenMax.to tween
+					.setTween(logoTimeline) // trigger a sweenMax.to tween
 					//.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
 
 
 // Switching titles to blue
 
-	new ScrollMagic.Scene({triggerElement: "#one .title", triggerHook: 0.3, duration: 500})
+	new ScrollMagic.Scene({triggerElement: "#one .title", triggerHook: 0.5, duration: 500})
 					.setClassToggle("#one .title", "blue-text")
 					.addTo(controller);
 
-	new ScrollMagic.Scene({triggerElement: "#two .title", triggerHook: 0.3, duration: 500})
+	new ScrollMagic.Scene({triggerElement: "#two .title", triggerHook: 0.5, duration: 500})
 					.setClassToggle("#two .title", "blue-text")
 					.addTo(controller);
 
-	new ScrollMagic.Scene({triggerElement: "#three .title", triggerHook: 0.3, duration: 500})
+	new ScrollMagic.Scene({triggerElement: "#three .title", triggerHook: 0.5, duration: 500})
 					.setClassToggle("#three .title", "blue-text")
 					.addTo(controller);
 
-	new ScrollMagic.Scene({triggerElement: "#four .title", triggerHook: 0.3, duration: 500})
+	new ScrollMagic.Scene({triggerElement: "#four .title", triggerHook: 0.5, duration: 500})
 					.setClassToggle("#four .title", "blue-text")
 					.addTo(controller);
 
-	new ScrollMagic.Scene({triggerElement: "#five .title", triggerHook: 0.3, duration: 500})
+	new ScrollMagic.Scene({triggerElement: "#five .title", triggerHook: 0.5, duration: 500})
 					.setClassToggle("#five .title", "blue-text")
 					.addTo(controller);
 
-	new ScrollMagic.Scene({triggerElement: "#seven .title", triggerHook: 0.3, duration: 500})
+	new ScrollMagic.Scene({triggerElement: "#seven .title", triggerHook: 0.5, duration: 500})
 					.setClassToggle("#seven .title", "blue-text")
 					.addTo(controller);
 
@@ -112,7 +68,7 @@ var isInViewport = function (elem) {
 
 // Smooth Scroll
 
-$('#logo-corner').click(function() {      // When arrow is clicked
+$('.logo').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
     }, 1000);
